3231d89266ccc854c4e61f4a83ff5db78a478bbd
dmitriy_krasnov <dmitriy_krasnov@outlook.com>
2025-03-06 15:17:41 +0300

    Updated annotations to correlation heatmap

--- thermograms_analysis/correlation.py
+++ thermograms_analysis/correlation.py
@@ -34,21 +34,30 @@ plt.close()
 classes = ['hu', 'hg', 'he','hp', 'hs', 'hm', 'hi']
 #classes = ['hi']
 out_corr = []
+out_p = []
 for cl in classes:
     data, y = prepare_dataset('thermograms_analysis/metrics_40.json', class_id=cl, clf=False)
     data = data.to_numpy()
     y = y.to_numpy()
     for i in range(data.shape[1]):
-        corr = spearmanr(data[:, i], y).statistic
-        out_corr.append(corr)
+        corr = spearmanr(data[:, i], y)
+        out_corr.append(corr.statistic)
+        out_p.append(corr.pvalue)
 
 
 out_corr = np.array(out_corr).reshape(len(classes), -1)
-
+out_p = np.array(out_p).reshape(len(classes), -1)
+# add anotations to heatmap
+annot = out_corr.round(3).astype(str)
+for p in (0.05, 0.01, 0.001):
+    annot = np.where(out_p < p, np.char.add(annot, '*'), annot)
+columns = ['\n'.join(col.split('_')) for col in columns]
 plt.figure(figsize=(10, 10))
-ax = sns.heatmap(out_corr, linewidths=1, xticklabels=columns, yticklabels=classes, annot=True)
+ax = sns.heatmap(out_corr, linewidths=1, xticklabels=columns, yticklabels=classes, annot=annot, fmt='', square=True, cbar_kws={"shrink": 0.65})
 ax.set_xlabel('Features')
 ax.set_ylabel('Defects')
+print(out_p)
 plt.yticks(rotation=0)
-plt.xticks(rotation=20)
+plt.xticks(rotation=0)
+plt.tight_layout()
 plt.savefig('thermograms_analysis/figures/correlation.jpg')
