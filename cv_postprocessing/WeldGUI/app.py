1201d0f13a8ea958d2f8a0d514688a731e091ec2
aveen007 <aveen2000hussein@gmail.com>
2025-10-03 13:10:25 +0300

    fix port

--- cv_postprocessing/WeldGUI/app.py
+++ cv_postprocessing/WeldGUI/app.py
@@ -1,5 +1,12 @@
-# WeldGUI/app.py
-from welding.api import app 
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return {"message": "Hello from Flask on Hugging Face!"}
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=7860)
+    port = int(os.environ.get("PORT", 7860))  # fallback to 7860 if PORT is unset
+    app.run(host="0.0.0.0", port=port)
